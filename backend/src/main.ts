c18bc0b9893a735dd9c1f522a3d7e53897490153
savinaDaria <darianna697@gmail.com>
2024-04-01 23:47:47 +0300

    fixed cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,11 +7,7 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
