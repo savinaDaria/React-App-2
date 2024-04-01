fd7dad227325666e342596b2f198b4fb14793598
savinaDaria <darianna697@gmail.com>
2024-04-01 22:50:07 +0300

    update path

--- backend/src/utils/database-config/typeorm.config.ts
+++ backend/src/utils/database-config/typeorm.config.ts
@@ -13,7 +13,7 @@ export function getTypeOrmConfig(config: ConfigService): TypeOrmModuleOptions {
     database: config.get('POSTGRES_DATABASE'),
     entities: ['src/modules/**/*.entity.js'],
     migrationsTableName: 'migration',
-    migrations: ['src/migrations/*.js'],
+    migrations: ['src/migrations/*.js','src/migrations/*.ts'],
     autoLoadEntities: true,
     ssl: false
   };
